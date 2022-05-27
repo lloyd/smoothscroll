22903cdac093f4bed387f0f7eea7620fee239aca
Lloyd Hilaiel <lloyd@hilaiel.com>
2022-05-27 13:41:52 -0600

    make SCROLL_TIME configurable

--- src/smoothscroll.js
+++ src/smoothscroll.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // polyfill
-function polyfill() {
+function polyfill(args) {
   // aliases
   var w = window;
   var d = document;
@@ -16,7 +16,7 @@ function polyfill() {
 
   // globals
   var Element = w.HTMLElement || w.Element;
-  var SCROLL_TIME = 468;
+  var SCROLL_TIME = args && args.scrollTime || 468;
 
   // object gathering original scroll methods
   var original = {
